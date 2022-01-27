456f0ebf86a0cb3d86f95b75cf7d48fa94048704
Michelle Kendall <michelle.kendall@warwick.ac.uk>
2022-01-27 10:08:56 +0000

    Latest data

--- ui.R
+++ ui.R
@@ -67,8 +67,7 @@ ui <- fluidPage(
           
           h2("Weekly measure of the percentage of all cases in England and Wales from the government dashboard which are reported through the app"),
           
-          h5("We note that the government dashboard measure does not yet include reinfections, whereas positive cases through the app could be reinfections,
-             so this measure is something of an overestimate, particularly since the arrival of the Omicron variant."),
+          h3("Please note that the government dashboard data and app data are not perfectly comparable, and that this measure may have become more unreliable since December 2021. Please see below for details."),
           
           withSpinner(plotlyOutput("percent_app_plot", height="80vh"), type=7),
           
@@ -76,11 +75,30 @@ ui <- fluidPage(
           
           h2("Weekly measure of the percentage of all cases amongst individuals aged 16 and over (eligible to use the app) in England and Wales which are reported through the app"),
           
-          h5("We note that the government dashboard measure does not yet include reinfections, whereas positive cases through the app could be reinfections,
-             so this measure is something of an overestimate, particularly since the arrival of the Omicron variant."),
+          h3("Please note that the government dashboard data and app data are not perfectly comparable, and that this measure may have become more unreliable since December 2021. Please see below for details."),
           
           withSpinner(plotlyOutput("percent_over_16_app_plot", height="80vh"), type=7),
           
+          h3("Comparisons to the government dashboard records of positive tests provide a rough measure of changing behaviours over time
+             but carry the following interpretational challenges:"),
+          h3("- The government dashboard data for England includes PCR and LFD positive tests, whereas for Wales only PCR tests are counted. 
+             The app data includes both PCR and LFD positive results. 
+             If an app user inputs multiple positive tests on the same day, these are counted once. 
+             However, for privacy reasons, it is not possible to distinguish if a user enters positive tests over multiple days, and they may therefore be counted repeatedly. 
+             These factors may be distorting the metric, particularly since the introduction of policies allowing 'early' release from self-isolation with
+             negative lateral flow results, which are likely to have increased the numbers of people taking lateral flow test in the days following a positive LFD or PCR."),
+          h3("- The government dashboard data includes reinfections (after six weeks) for Wales but not yest for England. 
+             Positive tests reported through the app may include reinfections. 
+             This will artificially inflate the apparent proportion of cases being reported through the app, particularly since the arrival of 
+             the Omicron variant where reinfections have become more common."),
+          
+          HTML("<h3>Full details of the government dashboard measure newCasesBySpecimenDate are provided here for 
+               <a href=\"https://coronavirus.data.gov.uk/metrics/doc/newCasesBySpecimenDate#england\" target=\"_blank\">England</a> and 
+               <a href=\"https://coronavirus.data.gov.uk/metrics/doc/newCasesBySpecimenDate#wales\" target=\"_blank\">Wales</a>
+               .</h3>"),
+          
+          br(),
+          br(),
           hr()
         )
       )
