925270e01cc3ee29068135175c282e352ece7698
Michelle Kendall <michelle.kendall@warwick.ac.uk>
2022-02-02 09:28:06 +0000

    Updating now that reinfections are included in England dashboard data

--- ui.R
+++ ui.R
@@ -82,15 +82,11 @@ ui <- fluidPage(
           h3("Comparisons to the government dashboard records of positive tests provide a rough measure of changing behaviours over time
              but carry the following interpretational challenges:"),
           h3("- The government dashboard data for England includes PCR and LFD positive tests, whereas for Wales only PCR tests are counted. 
-             The app data includes both PCR and LFD positive results. 
-             If an app user inputs multiple positive tests on the same day, these are counted once. 
-             However, for privacy reasons, it is not possible to distinguish if a user enters positive tests over multiple days, and they may therefore be counted repeatedly. 
-             These factors may be distorting the metric, particularly since the introduction of policies allowing 'early' release from self-isolation with
+             The app data includes both PCR and LFD positive results. "),
+          h3("- If an app user inputs multiple positive tests on the same day, these are counted as one positive test. 
+             However, for privacy reasons, it is not possible to distinguish if a user enters positive tests over multiple days, and they may therefore be counted repeatedly."), 
+          h3("These factors may be distorting the metric, particularly since the introduction of policies on 22 December 2021 and 17 January 2022 allowing 'early' release from self-isolation with
              negative lateral flow results, which are likely to have increased the numbers of people taking lateral flow test in the days following a positive LFD or PCR."),
-          h3("- The government dashboard data includes reinfections (after six weeks) for Wales but not yest for England. 
-             Positive tests reported through the app may include reinfections. 
-             This will artificially inflate the apparent proportion of cases being reported through the app, particularly since the arrival of 
-             the Omicron variant where reinfections have become more common."),
           
           HTML("<h3>Full details of the government dashboard measure newCasesBySpecimenDate are provided here for 
                <a href=\"https://coronavirus.data.gov.uk/metrics/doc/newCasesBySpecimenDate#england\" target=\"_blank\">England</a> and 
