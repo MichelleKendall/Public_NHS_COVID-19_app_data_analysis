5c8d964ecdd89bfe010455c41f4615d787c9b479
Michelle Kendall <michelle.kendall@warwick.ac.uk>
2022-02-17 11:41:47 +0000

    Fixing typo

--- ui.R
+++ ui.R
@@ -86,7 +86,7 @@ ui <- fluidPage(
           h3("- If an app user inputs multiple positive tests on the same day, these are counted as one positive test. 
              However, for privacy reasons, it is not possible to distinguish if a user enters positive tests over multiple days, and they may therefore be counted repeatedly."), 
           h3("These factors may be distorting the metric, particularly since the introduction of policies on 22 December 2021 and 17 January 2022 allowing 'early' release from self-isolation with
-             negative lateral flow results, which are likely to have increased the numbers of people taking lateral flow test in the days following a positive LFD or PCR."),
+             negative lateral flow results, which are likely to have increased the numbers of people taking lateral flow tests in the days following a positive LFD or PCR result."),
           
           HTML("<h3>Full details of the government dashboard measure newCasesBySpecimenDate are provided here for 
                <a href=\"https://coronavirus.data.gov.uk/metrics/doc/newCasesBySpecimenDate#england\" target=\"_blank\">England</a> and 
